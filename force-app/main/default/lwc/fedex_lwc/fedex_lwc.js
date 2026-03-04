74ad1d7e74ed80636010006cf818d310c9e884a7
grantjlee <grantlee117@gmail.com>
2026-03-04 01:22:21 -0800

    Created FedEx tracking service LWC and associated Apex classes

--- force-app/main/default/lwc/fedex_lwc/fedex_lwc.js
+++ force-app/main/default/lwc/fedex_lwc/fedex_lwc.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getLatestScan from '@salesforce/apex/FedExTrackingService.getLatestScan';
 
-export default class Fedex_lwc extends LightningElement {}
+export default class Fedex_lwc extends LightningElement {
+
+    trackingNumber = '';
+    @track latestScan;
+    @track error;
+
+    handleChange(event) {
+        this.trackingNumber = event.target.value;
+    }
+
+    handleTrack() {
+        this.error = undefined;
+        this.latestScan = undefined;
+
+        getLatestScan({ trackingNumber: this.trackingNumber })
+            .then(result => {
+                this.latestScan = result;
+            })
+            .catch(err => {
+                this.error = err.body?.message || 'Unknown error';
+            });
+    }
+}
